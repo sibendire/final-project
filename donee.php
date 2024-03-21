591b2563172fce1f96ea0a19bffc9d5f88a91d7f
sibendire <sibendirejoshua@gmail.com>
2024-03-21 11:11:31 +0300

    Update the file to handle real functions

--- donee.php
+++ donee.php
@@ -9,21 +9,21 @@
 <body>
     <p class="paragraph">Lorem ipsum dolor sit amet consectetur, adipisicing elit
          Sit commodi repellendus adipisci sunt dolorem quis libero
-          provident dicta modi ratione iste in quaerat, beatae quibusdam
+          provident dicta modiratione iste in quaerat, beatae quibusdam
          cum expedita! Distinctio, exercitationem illo?</p>
          <?php
 include("php/config.php");
 if(isset($_POST['submit'])){
-    $donorName = $_POST['donerName'];
+    $donorName = $_POST['doneeName'];
     $email = $_POST['email'];
-    $items = $_POST['items'];
+    $items = $_POST['item'];
     $number = $_POST['number'];
     $date = $_POST['date'];
-    $location = $_POST['location'];
+    $location = $_POST['address'];
     $contact = $_POST['contact'];
     
        // Verifying the unique email
-       $verify_query = mysqli_query($con, "SELECT Email FROM users WHERE Email='$email'");
+       $verify_query = mysqli_query($con, "SELECT Email FROM donee WHERE Email='$email'");
        if(mysqli_num_rows($verify_query) != 0){
            echo "<div class='message'>
            <p>This email is already in use. Please try another one.</p>
@@ -31,7 +31,7 @@ if(isset($_POST['submit'])){
            echo "<a href='javascript:history.back()'><button class='btn'>Go Back</button></a>";
        }
        else{
-           mysqli_query($con, "INSERT INTO donation(DonorName, Email, Items,Number,Date,Location,Contact) VALUES('$donorName','$email','$items','$number','$date','$location','$contact')") or die("Error occurred while inserting data: " . mysqli_error($con));
+           mysqli_query($con, "INSERT INTO donee(DoneeName, Email, Item,Number,Date,Address,Contact) VALUES('$donorName','$email','$items','$number','$date','$location','$contact')") or die("Error occurred while inserting data: " . mysqli_error($con));
            echo "<div class='message'>
            <p>Your content saved successfully.</p>
            </div><br>";
@@ -39,42 +39,42 @@ if(isset($_POST['submit'])){
        }
       
    }
-   else{
+    else{
    ?>
          <div class="donation">
          <form action="" method="post">
             <div class="container">
                 <h1>Donee</h1>
-                <p>Please fill in this form  to post your reqest</p>
+                <p>Please fill in this form  to post your request</p>
         
                 <label for="doneeName">Donee name</label>
-                <input type="text" placeholder="Enter Deneename" name="doneeName" required>
+                <input type="text" placeholder="Enter Donee Name" name="doneeName" required>
         
                 <label for="email">Email</label>
                 <input type="text" placeholder="Enter Email" name="email" required>
         
-                <label for="items">Item name donated</label>
-                <input type="text" placeholder="Enter item name" name="items" required>
+                <label for="item">Item name received</label>
+                <input type="text" placeholder="Enter item name" name="item" required>
         
-                <label for="number">Enter number of items donated</label>
-                <input type="number" placeholder="Number of items donate" name="number" required>
+                <label for="number">Enter number of items received</label>
+                <input type="number" placeholder="Number of items donated" name="number" required>
         
                 <label for="date">Enter the date of Donation</label>
-                <input type="datetime" placeholder="chose the date" name="date" required>
+                <input type="date" placeholder="Choose the date" name="date" required>
                   
-                <label for="location">Enter your location</label>
-                <input type="text" placeholder="Enter location" name="location" required>
+                <label for="address">Enter your location</label>
+                <input type="text" placeholder="Enter location" name="address" required>
 
                 <label for="contact">Please Enter your contact number</label>
-                <input type="number" placeholder="Number of items donate" name="contact" required>
+                <input type="number" placeholder="Contact number" name="contact" required>
                    
                 <div class="clearfix">
                     <button type="button" class="cancelbtn">Cancel</button>
-                    <button type="submit" class="signupbtn">update</button>
+                    <button type="submit" class="signupbtn" name="submit">Submit</button>
                 </div>
             </div>
         </form> 
     </div>
-    <?php}?>
+     <?php } ?> 
 </body>
 </html>
--- home.php
+++ home.php
@@ -11,21 +11,108 @@ if (!isset($_SESSION['valid'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Home</title>
 </head>
 <body>
-    <ul>
+
+
+<div class="containers">
+            <nav class="nav-bar">
+                <div class="left-item">
+                    <div>
+                        <a href="home.php">Home</a>
+                        <a href="donee.php">Donnee</a>
+                        <a href="donation.php">Donor</a>
+                        <a href="login.php">Login</a>
+                       
+                </div>
+</dev>
+    
+            </nav>
+</div>
+    <!-- <ul>
         <li><a href="home.php">Home</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="donee.php">Donee</a></li>
+        <li><a href="donation.php">Donor</a></li>
         <li style="float:center"><a href="#donate">Donate</a></li>
         <li style="float:right"><a class="active" href="login.php">Login</a></li>
-    </ul>
+    </ul> -->
+    <div class="im">
     <h2>HTML Image</h2>
+</div>
     <img src="Images/needy5.jpg" alt="Trulli" width="850" height="400px">
-    <div class="p">
+    <div class="more">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque molestiae, doloremque officiis expedita quia, pariatur sapiente voluptatibus repudiandae reprehenderit repellat omnis nisi ab possimus dolore commodi unde minus, error est!
     </div>
+
+    <button type="donate">Donate</button>
+    <div class="photo">
+        <br>
+        <p class="heading">Our Works</p>
+        <br>
+        <p style="font-size: 28px; text-align: center;">"Look what we can do together."</p>
+       <br>
+        <div class="wrapper">
+          <div class="box"><img src="images/needy1.jpg" alt=""></div>
+          <div class="box"><img src="images/needy5.jpg" alt=""></div>
+          <div class="box"><img src="images/needy7.jpg" alt=""></div>
+        </div>
+        <!-- <p style="font-size: 19px;"> The basic concept of this project  Food Waste Management is to collect theexcess/leftover food from donors such as hotels, restaurants, marriage halls, etc and distribute to  the  needy people .
+        </p> -->
+        <br>
+        
+    
+     
+        
+    </div>
+
+<!-- FOOTER SECTION -->
+    <footer class="footer">
+        <div class="footer-left col-md-4 col-sm-6">
+            <p class="about">
+                <span> About us</span>The core principle driving our initiative is the seamless connection between surplus resources and those in need. With intuitive interfaces and community engagement, we aim to facilitate impactful giving, uniting hearts for a better tomorrow
+            </p>
+        
+        </div>
+        <div class="footer-center col-md-4 col-sm-6">
+          <div>
+            <p><span> Contact</span> </p>
+            
+          </div>
+          <div>
+        
+            <p> (+256)708332127</p>
+            
+          </div>
+          <div>
+            <!-- <i class="fa fa-envelope" style="font-size: 17px;
+            line-height: 38px; color:white;"></i> -->
+            <p><a href="#"> nkbksharon@gmail.com</a></p>
+          </div>
+          
+          <div class="sociallist">
+            <ul class="social">
+            <li><a href="https://www.facebook.com/TheAkshayaPatraFoundation/"><img src="https://i.ibb.co/x7P24fL/facebook.png"></a></li>
+            <li><a href="https://twitter.com/globalgiving"><img src="https://i.ibb.co/Wnxq2Nq/twitter.png"></a></li>
+            <li><a href="https://www.instagram.com/charitism/"><img src="https://i.ibb.co/ySwtH4B/instagram.png"></a></li>
+            <li><a href="https://web.whatsapp.com/"><i class="fa fa-whatsapp" style="font-size:50px;color: black;"></i></a></li>
+           </ul>
+          </div>
+        </div>
+        <div class="footer-right col-md-4 col-sm-6">
+          <h2> DONATE<span> TODAY</span></h2>
+          <!-- <h2>Food donate</h2> -->
+          <p class="menu">
+            <a href="home.php"> Home</a> |
+            <a href="donation.php"> Donor</a> |
+            <a href="profile.php"> Profile</a> |
+            <a href="contact.html"> Contact</a>
+          </p>
+          <p class="name"> DONATE TO THE NEEDY &copy 2024</p>
+        </div>
+      </footer>
+
 </body>
 </html>
